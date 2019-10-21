46d9f45165831646b242b6b9b327a0bd40c5ddd3
Haider Khan <haiderkhan.live@gmail.com>
2019-10-21 20:03:59 +0500

    change form validations

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,26 +1,18 @@
 from django import forms
-from django.core.validators import validate_email
 from django.contrib.auth.models import User
-from .exception import DuplicateEmailError
 from django.contrib.auth.hashers import make_password
+from .mixins import EmailDuplicationMixin
 
 
-class SignUpForm(forms.ModelForm):
+class SignUpForm(EmailDuplicationMixin, forms.ModelForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(max_length=254,required=True)
     password = forms.CharField(widget=forms.PasswordInput, min_length=8)
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
-
-    def email_clean(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).exists():
-            raise DuplicateEmailError()
-        return email
 
     def save(self):
         user = super(SignUpForm, self).save(commit=False)
@@ -36,18 +28,11 @@ class LoginForm(forms.ModelForm):
         fields = ('username', 'password')
 
 
-class UpdateProfileForm(forms.ModelForm):
+class UpdateProfileForm(EmailDuplicationMixin, forms.ModelForm):
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
-
-    def email_clean(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).exists():
-            raise DuplicateEmailError()
-        return email
 
     def save(self):
         user = super(UpdateProfileForm, self).save(commit=False)
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,6 @@ from django.views import View
 from .forms import SignUpForm, UpdateProfileForm
 from django.http import HttpResponseRedirect
 from django.db.utils import IntegrityError
-from .exception import DuplicateEmailError
 # Create your views here.
 
 
@@ -15,54 +14,34 @@ class AuthenticationView(View):
 class SignUpView(View):
     def get(self, request):
         form = SignUpForm()
-
         return render(request, 'registration/signup.html', {'form': form})
 
     def post(self, request):
-
         form = SignUpForm(request.POST)
-        print(form.is_valid())
         if form.is_valid():
             try:
-                form.email_clean()
                 form.save()
                 return HttpResponseRedirect('/auth/login/')
             except IntegrityError as e:
                 return render(request, 'registration/signup.html', {'form': form, 'error': str(e).split('"')[1]})
-            except DuplicateEmailError as e:
-                return render(request, 'registration/signup.html', {'form': form, 'error': e})
         else:
-            form = SignUpForm(request.POST)
             return render(request, 'registration/signup.html', {'form': form})
 
 
 class ProfileUpdateView(View):
 
     def get(self, request):
-        form_data = {}
-        pre_user_data = {
-            'username': request.user.username,
-            'email': request.user.email,
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name
-
-        }
-        form_data['form'] = UpdateProfileForm(pre_user_data)
-        return render(request, 'registration/profile-update.html', form_data)
+        context = dict()
+        context['form'] = UpdateProfileForm(instance=request.user)
+        return render(request, 'registration/profile-update.html', context)
 
     def post(self, request):
-        data = {}
         update_user_form = UpdateProfileForm(request.POST, instance=request.user)
         if update_user_form.is_valid():
             try:
-                update_user_form.email_clean()
                 update_user_form.save()
                 return HttpResponseRedirect('/')
-            except IntegrityError as e:
-                data['form'] = update_user_form
-                data['error'] = e
-                return render(request, 'registration/profile-update.html', data)
-            except DuplicateEmailError as e:
-                data['form'] = update_user_form
-                data['error'] = e
-                return render(request, 'registration/profile-update.html', data)
+            except IntegrityError:
+                return render(request, 'registration/profile-update.html', update_user_form)
+        else:
+            return render(request, 'registration/signup.html', {'form': update_user_form})
